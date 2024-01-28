2c8ec6002d24e6d21e6924708fe95869219909cd
Silindile Madlala <87993886+Patricia244@users.noreply.github.com>
2024-01-29 01:48:03 +0200

    update server

--- server.js
+++ server.js
@@ -1,21 +1,37 @@
 const express = require('express')
 const app = express()
-const bodyParser = require("body-parser")
+const path = require("path")
 require("dotenv").config()
+
+
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.DATABASE_URL);
 const connection = mongoose.connection;
 
-connection.once('open', function() {
-console.log("connected to the database")
-}).on('error', function(error) {
+connection.once('open', function () {
+    console.log("connected to the database")
+}).on('error', function (error) {
     console.log('Connect Error: ', error);
 });
 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static("public"));
+app.use(express.static("public/style.css"));
+app.use(express.json())
 
- app.use(express.json())
- app.use(bodyParser.urlencoded({extended:true}))
- const visitorsRoute  = require('./routes/visitors.js')
- app.use('/visitors',visitorsRoute)
-app.listen(3000,()=>console.log("server started"))
+const visitorsRoute = require('./routes/visitorsRoute.js')
+app.use('', visitorsRoute)
+
+
+app.get("/new_visitor", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+const adminRoute = require('./routes/adminRoutes.js')
+app.use('', adminRoute)
+app.get("/new_admin", (req, res) => {
+    res.sendFile(path.join(__dirname, "admin.html"));
+});
+
+app.listen(process.env.PORT, () => console.log(`server listening on http://localhost:${process.env.PORT}/new_visitor`))
