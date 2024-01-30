162a885d2115949b4ab172fceb845d48b44cb299
Silindile Madlala <87993886+Patricia244@users.noreply.github.com>
2024-01-30 15:15:12 +0200

    remove comments

--- routes/visitorsRoute.js
+++ routes/visitorsRoute.js
@@ -2,9 +2,6 @@ const express = require("express");
 const router = express.Router();
 const VisitorsModel = require("../model/visitorModel");
 
-//post a visito
-
-  
 router.post("/new_visitor", async (req, res) => {
 
     const { name, dateOfVisit, email, message } = req.body
@@ -15,22 +12,10 @@ router.post("/new_visitor", async (req, res) => {
         message: message,
     });
     try {
-        const newVisitor = await visitor.save().then((res) => {
-        });
-        res.status(201).send(newVisitor);
+        await visitor.save()
+        res.status(201).json({success:true,message:"form submitted successfully"});
     } catch (error) {
         res.status(400).json({ message: error.message });
-    }
-});
-
-
-//get a visitor
-router.get("/new_visitors", async (req, res) => {
-    try {
-        const visitors = await VisitorsModel.find();
-        res.status(201).send(visitors);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
     }
 });
 
