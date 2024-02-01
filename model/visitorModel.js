211b8a7273d077976713c950e366e6ce48983c6e
Silindile Madlala <87993886+Patricia244@users.noreply.github.com>
2024-02-01 16:45:51 +0200

    renove date

--- model/visitorModel.js
+++ model/visitorModel.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose")
 
 const visitorScheme = new mongoose.Schema({
     name: { type: String, required: true },
-    dateOfVisit:{type:Date,require:true,default:Date.now},
     email: { type: String, required: true },
     message: { type: String, required: true }
 
