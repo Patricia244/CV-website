08090aabae21d08c5c93d9ac5b3ad99dc27ef97e
Silindile Madlala <87993886+Patricia244@users.noreply.github.com>
2025-01-12 03:06:54 +0200

    finished skills section

--- src/script.js
+++ src/script.js
@@ -88,16 +88,9 @@ const swiper = new Swiper('.slider-wrapper', {
     clickable: true,
     dynamicBullets: true,
   },
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
   breakpoints: {
-    0: {
+    697: {
       slidesPerView: 1,
-    },
-    620: {
-      slidesPerView: 2,
     },
     1024: {
       slidesPerView: 3,
